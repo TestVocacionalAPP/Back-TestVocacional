98b950bb30fbedf50d2e142bf82f33741193e70a
Antón Figueroa Raúl Santiago <santiagoantonfigueroa@gmail.com>
2024-11-12 22:45:04 -0500

    properties local

--- src/main/java/GTns_TestV/Application.java
+++ src/main/java/GTns_TestV/Application.java
@@ -3,6 +3,7 @@ package GTns_TestV;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//todo esta
 @SpringBootApplication
 public class Application {
 
